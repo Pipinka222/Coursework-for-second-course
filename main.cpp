63d39881ee1a93230c81b00bdda3943c23ccccba
Pipinka222 <Nikitas0408@gmail.com>
2024-10-21 19:50:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@ int main() {
     SetConsoleCP(1251);
 
     int M, N;
-    std::cout << "Ââåäèòå êîëè÷åñòâî êîîðäèíàò: ";
+    std::cout << "Введите количество координат: ";
     std::cin >> M;
-    std::cout << "Ââåäèòå êîëè÷åñòâî ôóíêöèé: ";
+    std::cout << "Введите количество функций: ";
     std::cin >> N;
 
     double* x_values = new double[M];
     double* y_values = new double[M];
-    std::cout << "Ââåäèòå êîîðäèíàòû x:\n";
+    std::cout << "Введите координаты x:\n";
     for (int i = 0; i < M; ++i) {
         std::cin >> x_values[i];
     }
-    std::cout << "Ââåäèòå êîîðäèíàòû y:\n";
+    std::cout << "Введите координаты y:\n";
     for (int i = 0; i < M; ++i) {
         std::cin >> y_values[i];
     }
@@ -30,19 +30,19 @@ int main() {
     matrixA.fill_array_A(x_values, M);
     matrixB.fill_array_B(x_values, y_values, N, M);
 
-    std::cout << "Ìàòðèöà À: \n";
+    std::cout << "Матрица А: \n";
     matrixA.print();
 
-    std::cout << "Ìàòðèöà B: \n";
+    std::cout << "Матрица B: \n";
     matrixB.print();
 
     Matrix adj_A = matrixA.adjugate();
     Matrix A_obr = adj_A;
-    std::cout << "Îáðàòíàÿ ìàòðèöà À: \n";
+    std::cout << "Обратная матрица А: \n";
     A_obr.print();
 
     Matrix result = A_obr * matrixB;
-    std::cout << "Ìàòðèöà êîýôôèöèåíòîâ: \n";
+    std::cout << "Матрица коэффициентов: \n";
     result.print();
 
     print_values(result, y_values, x_values, M);
