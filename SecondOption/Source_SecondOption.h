7e911ddda4566a2ca2f4c2cccb088ba61f339424
Pipinka222 <Nikitas0408@gmail.com>
2024-12-08 15:28:06 +0300

    Update Source_SecondOption.h

--- SecondOption/Source_SecondOption.h
+++ SecondOption/Source_SecondOption.h
@@ -1,49 +1,47 @@
-#ifndef COURSEWORK_H
-#define COURSEWORK_H
+#ifndef SOURCE_H
+#define SOURCE_H
 
-
+#include <vector>
 #include <iostream>
 #include <cmath>
 
 class Preparation {
 private:
     double** A;
-    double** B;
+    std::vector<double> B;
     int N, M;
 
 public:
-    Preparation(int n, int m) : N(n), M(m) {
+    Preparation(int n, int m) : N(n), M(m), B(n) {
         A = new double* [N];
-        B = new double* [N];
         for (int i = 0; i < N; ++i) {
             A[i] = new double[N];
-            B[i] = new double[1];
         }
     }
 
     ~Preparation() {
         for (int i = 0; i < N; ++i) {
             delete[] A[i];
-            delete[] B[i];
         }
         delete[] A;
-        delete[] B;
     }
 
     void fill_matrix_A(double x_values[]);
     void fill_matrix_B(double x_values[], double y_values[]);
-    double basis_function(int i, double x);
     double** get_matrix_A();
-    double** get_matrix_B();
+    const std::vector<double>& get_matrix_B() const;
     void print_matrix_A();
     void print_matrix_B();
+
+private:
+    double basis_function(int i, double x);
 };
 
 class Solution {
 private:
     double** A;
-    double** B;
-    double** result;
+    std::vector<double> B;
+    std::vector<double> result;
     int N;
 
     double det(double** matrix, int size);
@@ -51,23 +49,11 @@ private:
     double** find_adjugate();
 
 public:
-    Solution(double** a, double** b, int n) : A(a), B(b), N(n) {
-        result = new double* [N];
-        for (int i = 0; i < N; ++i) {
-            result[i] = new double[1];
-        }
-    }
-    ~Solution() {
-        for (int i = 0; i < N; ++i) {
-            delete[] result[i];
-        }
-        delete[] result;
-    }
-
+    Solution(double** a, const std::vector<double>& b, int n) : A(a), B(b), N(n), result(n, 0.0) {}
     void solve();
-    double** get_result();
+    const std::vector<double> get_result() const;
 };
 
-void print_result(double* x_values, double* y_values, int M, double** result);
+void print_result(double* x_values, double* y_values, int M, std::vector<double> result);
 
-#endif 
+#endif
